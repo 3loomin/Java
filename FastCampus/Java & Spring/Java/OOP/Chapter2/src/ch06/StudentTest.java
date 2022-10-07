17afec795f845438547a1ed04b2fc544fcdf6b27
3loomin <3loomin@gmail.com>
2022-10-07 20:56:34 +0900

    생성자에 매개변수를 넣어주고 this 명령어를 통해 멤버변수에 접근해 데이터를 넣는다

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter2/src/ch06/StudentTest.java
@@ -0,0 +1,9 @@
+package ch06;
+
+public class StudentTest {
+
+    public static void main(String[] args){
+        Student student = new Student(12345, "Lee", 5);
+        System.out.println(student.getStudentInfo());
+    }
+}
