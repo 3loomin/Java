285c81d6ccfcf79f50bf3671e4561124ebb096f9
3loomin <3loomin@gmail.com>
2022-10-07 18:12:59 +0900

    class 파일에 메서드 구현해보기

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter2/src/ch03/FunctionTest.java
@@ -0,0 +1,30 @@
+package ch03;
+
+public class FunctionTest {
+    public static int addNum(int num1, int num2){
+
+        return num1 + num2;
+    }
+
+    public static void sayHello(String greeting){
+        System.out.println(greeting);
+    }
+
+    public static int calcSum(){
+        int sum = 0;
+        int i;
+        for(i = 0; i <= 100; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 20;
+        int total = addNum(n1, n2);
+        System.out.println(total);
+        sayHello("안녕 난 3loomin이야");
+        total = calcSum();
+        System.out.println(total);
+    }
+}
