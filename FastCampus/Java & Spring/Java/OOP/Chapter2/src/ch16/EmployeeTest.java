5117cdd725b1d249354e38a05d1f88a7f81c7455
3loomin <3loomin@gmail.com>
2022-10-08 19:58:47 +0900

    static 변수는 인스턴스와 상관없는 영역에 메모리를 차지하고, 데이터를 공유할 수 있다.

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter2/src/ch16/EmployeeTest.java
@@ -0,0 +1,18 @@
+package ch16;
+
+public class EmployeeTest {
+    public static void main(String[] args){
+        Employee employee = new Employee();
+        employee.setEmployeeName("이순신");
+        //System.out.println(Employee.serialNum);
+
+        Employee employee1 = new Employee();
+        employee1.setEmployeeName("김유신");
+
+
+        System.out.println(employee.getEmployeeName() + "님의 사번은" + employee.getEmployeeId());
+        System.out.println(employee1.getEmployeeName() + "님의 사번은" + employee1.getEmployeeId());
+
+        //두개의 인스턴스가 하나의 메모리를 공유한다
+    }
+}
