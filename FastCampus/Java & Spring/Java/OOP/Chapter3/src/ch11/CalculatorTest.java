c6a543f2f7c06292ff35a7b5e9a267f2aa6ad422
3loomin <3loomin@gmail.com>
2022-10-15 18:29:24 +0900

    interface의 구현, 모든 메서드는 public abstract, 모든 변수는 public static final로 선언

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter3/src/ch11/CalculatorTest.java
@@ -0,0 +1,18 @@
+package ch11;
+
+public class CalculatorTest {
+    public static void main(String[] args){
+
+        int num1 = 10;
+        int num2 = 2;
+
+        Calc calc = new CompleteCalc();
+        System.out.println(calc.add(num1,num2));
+        System.out.println(calc.sub(num1,num2));
+        System.out.println(calc.mul(num1,num2));
+        System.out.println(calc.div(num1,num2));
+
+        CompleteCalc cc = (CompleteCalc)calc;
+        cc.showInfo();
+    }
+}
