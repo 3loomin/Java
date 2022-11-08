6029a245b83213f034bcc3c3d26d2dcceec78fe7
3loomin <3loomin@gmail.com>
2022-11-08 16:11:46 +0900

    HashSet을 이용할때 집합이므로 중복을 허용하면 안된다. 그래서 한 객체에 대한 집합을 구현하기 위해선 그 객체에 equals 와 hashcode를 이용하여 중복의 정의를 내려줘야 한다.

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter5/src/ch13/Member.java
@@ -0,0 +1,57 @@
+package ch13;
+
+import java.util.Objects;
+
+public class Member {
+    private int memberId;
+    private String memberName;
+
+    public Member(int memberId, String memberName){
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", memberName='" + memberName + '\'' +
+                '}';
+    }
+
+    //HashSet에서는 중복을 정의 내리기 위해 equals랑 hashcode를 override해줘야 한다!
+
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Member){
+            Member member = (Member)o;
+            if(this.memberId == member.memberId){
+                return true;
+            }
+            else return false;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return memberId;
+    }
+}
---
+++ FastCampus/Java & Spring/Java/OOP/Chapter5/src/ch13/MemberHashSetTest.java
@@ -0,0 +1,22 @@
+package ch13;
+
+public class MemberHashSetTest {
+     public static void main(String[] args){
+        MemberHashSet memberHashSet = new MemberHashSet();
+
+        Member mem1 = new Member(1001,"David");
+        Member mem2 = new Member(1002,"John");
+        Member mem3 = new Member(1003,"Tom");
+        //Member mem4 = new Member(1004,"Daniel");
+         Member mem4 = new Member(1003,"Daniel");
+
+         memberHashSet.addMember(mem1);
+         memberHashSet.addMember(mem2);
+         memberHashSet.addMember(mem3);
+         memberHashSet.addMember(mem4);
+
+         memberHashSet.showAllMember();
+         memberHashSet.removeMember(mem1.getMemberId());
+         memberHashSet.showAllMember();
+    }
+}
