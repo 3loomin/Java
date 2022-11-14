f33613f82ed52efd16c40c546bc27b1efbd7d313
3loomin <3loomin@gmail.com>
2022-11-14 17:29:23 +0900

    함수형 인터페이스를 활용하여 람다식 구현하기

--- FastCampus/Java & Spring/Java/OOP/Chapter6/src/ch02/Add.java
+++ FastCampus/Java & Spring/Java/OOP/Chapter6/src/ch02/Add.java
@@ -1,5 +1,7 @@
 package ch02;
 
+@FunctionalInterface
 public interface Add{
     public int add(int x, int y);
+    //public int sub(int x , int y); @FunctionalInterface 어노테이션 때문에 한 인터페이스엔 하나의 메서드만 존재해야 된다!
 }
---
+++ FastCampus/Java & Spring/Java/OOP/Chapter6/src/ch03/MyNumber.java
@@ -0,0 +1,6 @@
+package ch03;
+
+@FunctionalInterface
+public  interface MyNumber {
+    int getMax(int a, int b);
+}
