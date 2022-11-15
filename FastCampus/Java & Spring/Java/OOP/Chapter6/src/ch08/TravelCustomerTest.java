5a5b63f32a2d0d787767e28db708cfe6f26db589
3loomin <3loomin@gmail.com>
2022-11-15 17:20:37 +0900

    stream을 활용한 패키지 여행 비용 계산하기

---
+++ FastCampus/Java & Spring/Java/OOP/Chapter6/src/ch08/TravelCustomerTest.java
@@ -0,0 +1,29 @@
+package ch08;
+
+import javax.swing.plaf.basic.BasicSliderUI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TravelCustomerTest {
+    public static void main(String[] args){
+        TravelCustomer customer1 = new TravelCustomer("Lee",40,100);
+        TravelCustomer customer2 = new TravelCustomer("Kim",20,100);
+        TravelCustomer customer3 = new TravelCustomer("Hong",13,50);
+
+        List<TravelCustomer> customerList = new ArrayList<TravelCustomer>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+        customerList.add(customer3);
+
+        System.out.println("Print customer List");
+        customerList.stream().map(c-> c.getName()).forEach(s-> System.out.println(s));
+
+        System.out.println("travel price");
+        System.out.println(customerList.stream().mapToInt(c->c.getPrice()).sum());
+
+        System.out.println("sorted name that older than 20 years old");
+        customerList.stream().filter(c->c.getAge() >= 20).map(c->c.getName()).sorted().forEach(s-> System.out.println(s));
+
+
+    }
+}
