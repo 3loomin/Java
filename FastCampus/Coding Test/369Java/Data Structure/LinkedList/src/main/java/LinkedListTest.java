0ca6e10582f304939b5ec2a66760aa7b8ed2941e
3loomin <3loomin@gmail.com>
2023-01-05 17:59:12 +0900

    :sparkles: 연결 리스트 뒤에 추가하는 addNode()기능 추가

---
+++ FastCampus/Coding Test/369Java/Data Structure/LinkedList/src/main/java/LinkedListTest.java
@@ -0,0 +1,12 @@
+public class LinkedListTest {
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+
+        MyLinkedList.printAll();
+
+
+    }
+}
---
+++ FastCampus/Coding Test/369Java/Data Structure/LinkedList/src/main/java/Node.java
@@ -0,0 +1,8 @@
+public class Node<T> {
+    private T data;
+    Node<T> next = null;
+
+    public Node(T data){
+        this.data = data;
+    }
+}
