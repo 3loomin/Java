eb8026946595ba6a78efe323fffc2ee28e8ee3e9
3loomin <3loomin@gmail.com>
2023-02-08 19:33:48 +0900

    외부로부터 의존성을 주입 받음으로서 DIP를 준수할 수 있게 되었다.

--- Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,8 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
     // 의존관계 문제점 추상화와 구현체에 의존하고 있다.
     // dip를 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
