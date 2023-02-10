9fe43fd260a586afb761118ff516713122c65cfd
3loomin <3loomin@gmail.com>
2023-02-10 19:47:34 +0900

    스프링 컨테이너인 ApplicationContext를 사용하여 DI를 해준다. 스프링 컨테이너는 @Configuration이 붙은 AppConfig를 설정 정보로 사용한다. 여기서 @Bean이라 적힌 메서드를 모두 호출해 컨테이너에 등록한다. 이렇게 등록된 bean들을 getBean 메서드를 통해서 찾을 수 있다.

--- Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/AppConfig.java
+++ Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
         //return new FixDiscountPolicy();
         //할인 정책을 바꾼다면 여기만 수정
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
         //메모리에서 데이터 베이스를 쓴다면 이 부분만 변경하면 된다
     }
