b14bedadd423cc5908c6f0e714d216fd925baad9
3loomin <3loomin@gmail.com>
2023-02-10 18:59:09 +0900

    역할을 세우고 구현이 그 안에 들어가게 리팩토링해서 중복도 제거

--- Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/AppConfig.java
+++ Inflearn/Spring Core Basic/Order & Discount/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,21 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+        //할인 정책을 바꾼다면 여기만 수정
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+        //메모리에서 데이터 베이스를 쓴다면 이 부분만 변경하면 된다
     }
 
 }
